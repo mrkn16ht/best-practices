b57376a6a6d0ecbc293ac6f0fc896b97cc87b243
mrkn16ht <46416323+mrkn16ht@users.noreply.github.com>
2019-03-17 16:58:33 +0800

    Add files via upload

---
+++ medium1.py
@@ -0,0 +1,13 @@
+number=int(input("Enter maximum number: "))
+prime=[]
+
+for a in range(2,number+1):
+    factor=0
+    for i in range(2,a//2+1):
+        if(a%i==0):
+           factor=factor+1 
+    if(factor<=0):
+        prime.append(a)
+
+print(number, '-> ', end='')
+print(*prime, sep=' ')
